fda56fc1cea6031181df1983cc92eae254649c99
Caner Ates <cnrates@gmail.com>
2024-05-10 14:50:58 +0300

    Initial Commit

---
+++ Scrollable-Segmented-Control-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrollable-Segmented-Control-UIKit
+//
+//  Created by Caner Ates on 10.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
